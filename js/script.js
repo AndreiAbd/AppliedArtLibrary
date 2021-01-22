89df1043d63ecebe4773c505a67059f447ed3f91
Andrei Abd <usr.g0a1@gmail.com>
2021-01-22 12:52:38 +0200

    intial commit

---
+++ js/script.js
@@ -0,0 +1,398 @@
+//     Powered | CoderX ©2020 | Andrei Abd
+//     █▀▀ █▀█ █▀▄ █▀▀ █▀█ ▀▄▀
+//     █▄▄ █▄█ █▄▀ ██▄ █▀▄ █░█
+//     CODER X |تم تصميم الموقع و برمجته من قبل اندريه عبد
+//     mail      :  usr.g0a1@gmial.com
+//     CoderX    :  https://coderx-project.blogspot.com/
+//     Whatsapp  :  +963934820511
+//     Pinterest :  https://www.pinterest.com/andreiabd93
+//     Instagram :  https://www.instagram.com/andrei_abd/
+//     Linkeden  :  https://www.linkedin.com/in/andreiabd
+//     Facebook  :  https://www.facebook.com/andreiaabd/
+
+var
+btnSitePraivacy = document.getElementById("btn-site-pravicy"),
+btnContentInteriorDesign = document.getElementById("btn-content-interior-design"),
+btnInteriorDesign = document.getElementById("btn-interior-design"),
+btnFashion = document.getElementById("btn-fashion"),
+btnContentFashion = document.getElementById("btn-content-fashion"),
+btnContentCaligraph = document.getElementById("btn-content-caligraph"),
+btnCaligraph = document.getElementById("btn-caligraph"),
+btnContactDecoration = document.getElementById("btn-content-decoration"),
+btnDecoration = document.getElementById("btn-decoration"),
+btnAppliedArtBooks = document.getElementById("btn-applied-art-books"),
+btnContentColors = document.getElementById("btn-content-colors"),
+btnColors = document.getElementById("btn-color-books"),
+btnColor = document.getElementById("btn-color"),
+btnPhilasophyBook = document.getElementById("btn-philasophy-books"),
+btnContentPhilasopy = document.getElementById("btn-content-philasophy"),
+btnPhilasophy = document.getElementById("btn-philosophya-books");
+
+btnPhilasophy.onmouseup = ()=>{
+var x = document.getElementById("philosophy-books");
+var xx = document.getElementById("applied-art-books");
+var xxx = document.getElementById("pravicy-site");
+var xxxx = document.getElementById("fashion-books");
+var xxxxx = document.getElementById("interior-design-books");
+var xxxxxx = document.getElementById("caligraph-books");
+var xxxxxxx = document.getElementById("decoration-books");
+var xxxxxxxx = document.getElementById("color-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+    xxxxxxx.style.display = "none";
+    xxxxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+    xxxxxxx.style.display = "none";
+    xxxxxxxx.style.display = "none";
+}
+};
+
+btnAppliedArtBooks.onmouseup = ()=>{
+var x = document.getElementById("applied-art-books");
+var xx = document.getElementById("pravicy-site");
+var xxx = document.getElementById("fashion-books");
+var xxxx = document.getElementById("interior-design-books");
+var xxxxx = document.getElementById("caligraph-books");
+var xxxxxx = document.getElementById("decoration-books");
+var xxxxxxx = document.getElementById("color-books");
+var xxxxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+    xxxxxxx.style.display = "none";
+    xxxxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+    xxxxxxx.style.display = "none";
+    xxxxxxxx.style.display = "none";
+}
+};
+
+btnSitePraivacy.onclick = ()=>{
+var x = document.getElementById("pravicy-site");
+var xx = document.getElementById("fashion-books");
+var xxx = document.getElementById("interior-design-books");
+var xxxx = document.getElementById("caligraph-books");
+var xxxxx = document.getElementById("decoration-books");
+var xxxxxx = document.getElementById("color-books");
+var xxxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+    xxxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+    xxxxxxx.style.display = "none";
+}
+};
+
+btnContentInteriorDesign.onclick = ()=>{
+var x = document.getElementById("interior-design-content");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnContactDecoration.onclick = ()=>{
+var x = document.getElementById("decoration-content");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnContentColors.onclick = ()=>{
+var x = document.getElementById("color-content");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnContentFashion.onclick = ()=>{
+var x = document.getElementById("fashion-content");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnContentCaligraph.onclick = ()=>{
+var x = document.getElementById("caligraph-content");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnContentPhilasopy.onclick = ()=>{
+var x = document.getElementById("philasophy-content");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnFashion.onclick = ()=>{
+var x = document.getElementById("fashion-books");
+var xx = document.getElementById("interior-design-books");
+var xxx = document.getElementById("caligraph-books");
+var xxxx = document.getElementById("decoration-books");
+var xxxxx = document.getElementById("color-books");
+var xxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+}
+};
+
+btnPhilasophyBook.onclick = ()=>{
+var x = document.getElementById("philasophy-book");
+var xx = document.getElementById("fashion-books");
+var xxx = document.getElementById("interior-design-books");
+var xxxx = document.getElementById("caligraph-books");
+var xxxxx = document.getElementById("decoration-books");
+var xxxxxx = document.getElementById("color-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+}
+};
+
+
+btnInteriorDesign.onclick = ()=>{
+var x = document.getElementById("interior-design-books");
+var xx = document.getElementById("fashion-books");
+var xxx = document.getElementById("caligraph-books");
+var xxxx = document.getElementById("decoration-books");
+var xxxxx = document.getElementById("color-books");
+var xxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+}
+};
+
+btnCaligraph.onclick = ()=>{
+var x = document.getElementById("caligraph-books");
+var xx = document.getElementById("interior-design-books");
+var xxx = document.getElementById("fashion-books");
+var xxxx = document.getElementById("decoration-books");
+var xxxxx = document.getElementById("color-books");
+var xxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+}
+};
+
+btnDecoration.onclick = ()=>{
+var x = document.getElementById("decoration-books");
+var xx = document.getElementById("caligraph-books");
+var xxx = document.getElementById("interior-design-books");
+var xxxx = document.getElementById("fashion-books");
+var xxxxx = document.getElementById("color-books");
+var xxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+}
+};
+
+btnColor.onclick = ()=>{
+var x = document.getElementById("colors-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+};
+
+btnColors.onmouseup = ()=>{
+var x = document.getElementById("color-books");
+var xx = document.getElementById("decoration-books");
+var xxx = document.getElementById("caligraph-books");
+var xxxx = document.getElementById("interior-design-books");
+var xxxxx = document.getElementById("fashion-books");
+var xxxxxx = document.getElementById("philosophy-books");
+if (x.style.display == "none"){
+    x.style.display = "block";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+} else {
+    x.style.display = "none";
+    xx.style.display = "none";
+    xxx.style.display = "none";
+    xxxx.style.display = "none";
+    xxxxx.style.display = "none";
+    xxxxxx.style.display = "none";
+}
+};
+//back to top button
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+  if(!backToTopButton.classList.contains("btnEntrance")) {
+    backToTopButton.classList.remove("btnExit");
+    backToTopButton.classList.add("btnEntrance");
+    backToTopButton.style.display = "block";
+  }
+  }
+  else { // Hide backToTopButton
+  if(backToTopButton.classList.contains("btnEntrance")) {
+    backToTopButton.classList.remove("btnEntrance");
+    backToTopButton.classList.add("btnExit");
+    setTimeout(function() {
+    backToTopButton.style.display = "none";
+    }, 250);
+  }
+  }
+};
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+  if (!start) start = timestamp;
+  const progress = timestamp - start;
+  window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+  if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+  t /= d/2;
+  if (t < 1) return c/2*t*t*t + b;
+  t -= 2;
+  return c/2*(t*t*t + 2) + b;
+};
+
+function CustomAlert() { // About and Contact, Box Alert. 
+  this.render = function(dialog){
+      var winW = window.innerWidth;
+      var winH = window.innerHeight;
+      var dialogoverlay = document.getElementById("dialogoverlay");
+      var dialogbox = document.getElementById("dialogbox");
+      dialogoverlay.style.display = "block";
+      dialogoverlay.style.height = winH +"px";
+      dialogbox.style.left = (winW/2) - (250 * .5)+"px";
+      dialogbox.style.top = "50px";
+      dialogbox.style.display = "block";
+      document.getElementById("dialogboxbody").innerHTML = dialog;
+      document.getElementById("dialogboxfooter").innerHTML = '<button style="margin: 10px;border-radius: 5px;border: 1px solid black;color: #000;" onclick="Alert.ok()">حسناً</i></button>';
+  }
+  this.ok = function(){
+      document.getElementById("dialogbox").style.display = "none";
+      document.getElementById("dialogoverlay").style.display = "none";
+  }
+};
+var Alert = new CustomAlert();
+
+onload = ()=>{
+  Alert.render('<img src="gif/00-a.gif" width="40%" alt=""><br><p dir="rtl"><strong>مرحباً بك في مكتبة الفنون التطبيقية!</strong></p><p dir="rtl">المكتبة الإلكترونية المخصصة لأبحاث و مقالات الفنون التطبيقية بصفة عامة</p><strong><p>لا تنسى متابعة صفحة الموقع على <a href="https://www.facebook.com/appliedartlibrarybooks/" target="blank">الفيس بوك</a></p></strong>');
+}
